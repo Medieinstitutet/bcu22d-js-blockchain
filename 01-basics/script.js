06e44cddf91ba86cd7f2d50d9a4c37a015c3184b
Johan Nordström <johan@digitalvillage.se>
2022-10-24 14:17:55 +0200

    Variables and constants.

--- 01-basics/script.js
+++ 01-basics/script.js
@@ -1,3 +1,29 @@
-//alert("Hello, World!");
+// alert("Hello, World!");
 
-console.log("Hi, World!");
+// let msg = "Hello, World!";
+
+/*
+let msg;
+msg = "I'm created on the line below";
+
+console.log(msg);
+console.log(msg);
+console.log(msg);
+
+msg = "I overwrote my friend";
+console.log(msg);
+console.log(msg);
+*/
+
+let firstName = "Johan";
+const lastName = "Nordström";
+
+console.log(firstName);
+console.log(lastName);
+
+console.log(firstName + " " + lastName);
+
+firstName = "Pelle";
+lastName = "Svanslös";
+
+console.log("I will never be run");
