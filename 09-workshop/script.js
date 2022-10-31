845b3d588aeef5e068af4fdffdc2eed1b8c1fd76
Johan Nordström <johan@digitalvillage.se>
2022-10-31 13:51:58 +0100

    Solution for Password Check workshop.

--- 09-workshop/script.js
+++ 09-workshop/script.js
@@ -9,13 +9,15 @@
  */
 
 let password;
-password = "password"; // inte giltigt
+// password = "password"; // inte giltigt
 // password = "pa$sword"; // giltigt
-// password = "p@ssw%rd"; // giltigt
+password = "p@ssw%rd"; // giltigt
 // password = "pa$$word"; // giltigt
 // password = "secretpassword"; // inte giltigt
 // password = "secret-password"; // giltigt
 // password = "such-password-much-secure-very-long"; // giltigt
+
+console.log(`🕵🏻 Checking password '${password}'`);
 
 const specialChars = [
 	"@",
@@ -35,3 +37,33 @@ const specialChars = [
 	"}",
 	"'",
 ];
+
+let hasSpecialChar = false;
+// FÖR varje specialtecken i listan,
+// KOLLA om lösenordet innehållet det specialtecken vi är på
+// OM lösenordet innehåller specialtecknet, ÄNDRA hasSpecialChar till TRUE
+// ANNARS gör ingenting
+for (let i = 0; i < specialChars.length; i++) {
+	const specialChar = specialChars[i];
+	console.log(`Loop ${i}, password contains ${specialChar}?`, password.includes(specialChar));
+
+	if (password.includes(specialChar)) {
+		console.log(`YAY FOUND SPECIAL CHAR!!!!!!!!!! 🥳🥳🥳🥳`);
+		hasSpecialChar = true;
+	}
+}
+
+
+if (password.length >= 16) {
+	console.log("- ✅ Great! That's a long password!");
+
+} else if (password.length >= 12 && password.includes('-')) {
+	console.log("- ✅ Great! That's a pretty good password!");
+
+} else if (password.length >= 8 && hasSpecialChar) {
+	console.log("- ✅ Great! Such password, much secure, very hard to crack!");
+
+} else {
+	console.log("- 🚨 Insecure password, my grandma can crack it!");
+
+}
