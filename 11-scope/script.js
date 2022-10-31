0e7156b2b8d498cebdd7a54069c491b418dbd148
Johan Nordström <johan@digitalvillage.se>
2022-10-31 16:02:04 +0100

    Global and Block Scope.

---
+++ 11-scope/script.js
@@ -0,0 +1,54 @@
+/**
+ * Scope
+ */
+
+/*
+let points = 50;  // globally scoped variable
+console.log("points before if-statements:", points);
+
+if (true) {
+	let points = 40; // block scoped/locally scoped variable
+	console.log("points inside first if-statement:", points);
+
+	if (true) {
+		points = 1337;
+		console.log("points inside second if-statement:", points);
+	}
+
+	console.log("points inside first if-statement, but after second if-statement:", points);
+}
+
+console.log("points after if-statements:", points); // 50
+*/
+
+/**
+ * MORE SCOPE!
+ */
+/*
+if (true) {
+	let msg = "the matrix has you";
+}
+console.log(msg);
+*/
+
+/*
+var points = 50;
+// var points = 1337;
+console.log("points before if-statements:", points);
+
+if (true) {
+	var points = 40;
+	console.log("points inside first if-statement:", points);
+}
+console.log("points after if-statements:", points);
+*/
+
+/*
+if (true) {
+	var msg = "the matrix has you";
+}
+console.log(msg);
+*/
+
+console.log(msg);
+var msg = "the matrix has you";
