be978de619d071d8903cabe2259ceb07600e8bc8
Johan Nordström <johan@digitalvillage.se>
2022-11-02 15:47:03 +0100

    Prompt a user for input.

---
+++ 14-prompt/script.js
@@ -0,0 +1,10 @@
+/**
+ * Prompt 🙋🏼
+ */
+
+let username = prompt("Please enter your name");
+if (username) {
+	alert(`Hello ${username}, nice to meet you`);
+} else {
+	alert("Hello anonymous visitor");
+}
