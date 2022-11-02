c198ef574439f102a5ea7ba3e951aa940d6d4861
Johan Nordström <johan@digitalvillage.se>
2022-11-02 15:17:49 +0100

    Random (😅) Math + another micro-workshop.

--- 13-math/script.js
+++ 13-math/script.js
@@ -9,8 +9,45 @@
  * Returns false if `a` is even
  */
 const is_odd = (a) => {
+	/*
+	if (a % 2 === 1) {
+		// a is odd
+		return true;
+	} else {
+		// a is even
+		return false;
+	}
+	*/
 
+	/*
+	if (a % 2) {
+		// a is odd
+		return true;
+	} else {
+		// a is even
+		return false;
+	}
+	*/
+
+	return (a % 2)
+		? true
+		: false;
 }
 
 is_odd(3); // true
 is_odd(8); // false
+
+/**
+ * Useful(?) Math functions
+ *
+ * Math.round() - rounds a decimal number up or down
+ * Math.floor() - rounds a decimal number down
+ * Math.ceil()  - rounds a decimal number up
+ */
+
+/**
+ * Returns a random number between 1-10
+ */
+const getRandomNumber = () => {
+
+}
