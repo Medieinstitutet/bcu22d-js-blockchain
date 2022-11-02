c00f05c73c92491ccc559c3637b30037873311cb
Johan Nordström <johan@digitalvillage.se>
2022-11-02 14:27:27 +0100

    Micro-workshop: Test if a number is odd or not.

---
+++ 13-math/script.js
@@ -0,0 +1,16 @@
+/**
+ * Math 🧮😳
+ */
+
+/**
+ * Tests if `a` is an odd number
+ *
+ * Returns true if `a` is odd
+ * Returns false if `a` is even
+ */
+const is_odd = (a) => {
+
+}
+
+is_odd(3); // true
+is_odd(8); // false
